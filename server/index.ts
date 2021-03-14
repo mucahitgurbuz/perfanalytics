62c813192548de404b65f5f72915600eec7807f6
Mücahit Gürbüz <mucahit.gurbuz@oplog.com.tr>
2021-03-14 19:14:50 +0300

    serve with https

--- server/index.ts
+++ server/index.ts
@@ -1,26 +1,33 @@
-import { createServer } from 'http';
-import { app } from './app';
-import path from 'path'
-import fs from 'fs';
-import { sequelize } from './db';
+import { createServer } from "https";
+import { app } from "./app";
+import path from "path";
+import fs from "fs";
+import { sequelize } from "./db";
 
-if (!fs.existsSync(path.join(__dirname, 'server-config.json'))) {
-  console.error('Configuration files are missing. Please run `yarn configure` first.');
+if (!fs.existsSync(path.join(__dirname, "server-config.json"))) {
+  console.error(
+    "Configuration files are missing. Please run `yarn configure` first."
+  );
   process.exit();
 }
 
-import config from './server-config.json';
+import config from "./server-config.json";
 
-const production = process.env.NODE_ENV === 'production';
+const production = process.env.NODE_ENV === "production";
 (async () => {
   await sequelize.sync({ force: !production });
-  if(!production){
-    require(path.resolve(__dirname, 'db', 'fillDummyContent'))(sequelize, () => {
-      createServer(app)
-      .listen(config.api.port, () => console.info(`listening on port ${config.api.port}`));
-    });
-  }else{
-    createServer(app)
-    .listen(config.api.port, () => console.info(`listening on port ${config.api.port}`));
+  if (!production) {
+    require(path.resolve(__dirname, "db", "fillDummyContent"))(
+      sequelize,
+      () => {
+        createServer(app).listen(config.api.port, () =>
+          console.info(`listening on port ${config.api.port}`)
+        );
+      }
+    );
+  } else {
+    createServer(app).listen(config.api.port, () =>
+      console.info(`listening on port ${config.api.port}`)
+    );
   }
 })();
